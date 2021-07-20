fbb6375d77c6f49e44956d9ac1b06c3c84677f98
Mitsuo Utano <utano320@gmail.com>
2021-07-20 22:56:12 +0900

    ABC114 A

---
+++ C++/abc114/a/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int64_t solve(int n, const vector<int64_t>& a) {
+  int64_t ans;
+
+  return ans;
+}
+
+int main() {
+  // input
+  int n; cin >> n;
+  if (n == 3 || n == 5 || n == 7) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
