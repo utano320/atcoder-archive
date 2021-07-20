5639b7d4b35501c6c67da50106d96b21306796ac
Mitsuo Utano <utano320@gmail.com>
2021-07-20 22:43:38 +0900

    ABC111 A

---
+++ C++/abc111/a/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  rep(i, 3) {
+    char c;
+    cin >> c;
+    if (c == '1') {
+      cout << '9';
+    } else {
+      cout << '1';
+    }
+  }
+
+  // output
+  cout << endl;
+}
