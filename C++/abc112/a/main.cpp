22a36eb354af54ff818cad5e0dfc6060dd41e25b
Mitsuo Utano <utano320@gmail.com>
2021-07-20 22:47:48 +0900

    ABC112 A

---
+++ C++/abc112/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+
+  if (n == 1) {
+    cout << "Hello World" << endl;
+  } else {
+    int a, b; cin >> a >> b;
+    cout << a + b << endl;
+  }
+}
