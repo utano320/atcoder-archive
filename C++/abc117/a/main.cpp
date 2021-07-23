0fe373e7776da843978e5f713f8264b7dfd284f4
Mitsuo Utano <utano320@gmail.com>
2021-07-24 05:22:35 +0900

    ABC117 A

---
+++ C++/abc117/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  double t, x; cin >> t >> x;
+
+  // output
+  cout << t / x << endl;
+}
