90f04c9813b0ca837c25c31d8985bc8f128d2f86
Mitsuo Utano <utano320@gmail.com>
2021-07-28 05:12:57 +0900

    ABC121 A

---
+++ C++/abc121/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int H, W, h, w; cin >> H >> W >> h >> w;
+
+  // output
+  cout << (H - h) * (W - w) << endl;
+}
