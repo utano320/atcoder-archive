dc4647fd5197a059ef5edd57406d5730abefba51
Mitsuo Utano <utano320@gmail.com>
2021-08-04 08:00:00 +0900

    ABC128 A

---
+++ C++/abc128/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int a, p; cin >> a >> p;
+
+  int ans = (a * 3 + p) / 2;
+  // output
+  cout << ans << endl;
+}
