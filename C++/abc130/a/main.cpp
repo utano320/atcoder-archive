d670b83b955502f8945d11a87022d1de4ebb25cd
Mitsuo Utano <utano320@gmail.com>
2021-08-06 10:46:51 +0900

    ABC130 A

---
+++ C++/abc130/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int x, a; cin >> x >> a;
+
+  int ans;
+  if (x < a) {
+    ans = 0;
+  } else {
+    ans = 10;
+  }
+  // output
+  cout << ans << endl;
+}
