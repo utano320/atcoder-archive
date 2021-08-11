809f3da3dcf508d333c81fcf1325bb72e32e2a3b
Mitsuo Utano <utano320@gmail.com>
2021-08-11 09:45:31 +0900

    ABC134 A

---
+++ C++/abc134/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+
+
+  // output
+  cout << 3 * n * n << endl;
+}
