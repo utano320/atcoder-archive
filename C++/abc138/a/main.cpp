14518c6d662c83421a04b7abb5524029aa43871e
Mitsuo Utano <utano320@gmail.com>
2021-08-14 16:47:57 +0900

    ABC138 A

---
+++ C++/abc138/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int a; cin >> a;
+  string s; cin >> s;
+
+  // output
+  cout << ((a >= 3200) ? s : "red") << endl;
+}
