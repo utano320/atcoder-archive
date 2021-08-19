3fa7f12cb891cb3f25a498fd96c18207cb072b5f
Mitsuo Utano <utano320@gmail.com>
2021-08-19 18:04:35 +0900

    ABC143 A

---
+++ C++/abc143/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int a, b; cin >> a >> b;
+
+  // output
+  cout << max(0, a - b * 2) << endl;
+}
