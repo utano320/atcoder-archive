b8db7c800af39e619a3fa1d18618db92546a0c09
Mitsuo Utano <utano320@gmail.com>
2021-08-21 16:11:28 +0900

    ABC145 A

---
+++ C++/abc145/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int r; cin >> r;
+
+  // output
+  cout << r * r << endl;
+}
