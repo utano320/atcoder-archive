7021f5c88bf4ab8278ed8c21be1dcd2ca5bbf1bf
Mitsuo Utano <utano320@gmail.com>
2021-08-26 20:10:50 +0900

    ABC150 A

---
+++ C++/abc150/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int k, m ; cin >> k >> m;
+  
+  cout << ((500*k>=m) ? "Yes" : "No") << endl;
+}
