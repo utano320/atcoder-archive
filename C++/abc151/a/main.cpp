3f5ba3a3fa42b1479897b574b6a7db9dca9ad489
Mitsuo Utano <utano320@gmail.com>
2021-08-27 16:56:58 +0900

    ABC151 A

---
+++ C++/abc151/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  char c; cin >> c;
+  
+  printf("%c", c + 1);
+  printf("\n");
+}
