48c12c6fba57f9fa634f8ae67924020ee5f44144
Mitsuo Utano <utano320@gmail.com>
2021-08-28 16:49:30 +0900

    ABC152 A

---
+++ C++/abc152/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n, m ; cin >> n >> m;
+  
+  cout << ((n == m) ?  "Yes" : "No") << endl;
+}
