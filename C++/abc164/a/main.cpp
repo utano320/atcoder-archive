b6905b2969f9094c4af690b42db9fcf0f959e74f
Mitsuo Utano <utano320@gmail.com>
2021-09-09 10:58:18 +0900

    ABC164 A

---
+++ C++/abc164/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int s, w ; cin >> s >> w;
+  
+  cout << ((w >= s) ? "unsafe" : "safe") << endl;
+}
