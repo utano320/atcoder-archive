67f53dcc909b7cbe1080161b0fdf1c6a58442571
Mitsuo Utano <utano320@gmail.com>
2021-09-12 13:35:37 +0900

    ABC167 A

---
+++ C++/abc167/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  string s,t ; cin >> s >> t;
+  
+  cout << ((s == t.substr(0, s.size())) ? "Yes" : "No") << endl;
+}
