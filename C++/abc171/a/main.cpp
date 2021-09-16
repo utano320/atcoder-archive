6ca616cf11b1b6b673ccfb7cfd8bf8674bf53bf2
Mitsuo Utano <utano320@gmail.com>
2021-09-16 18:25:34 +0900

    ABC171 A

---
+++ C++/abc171/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  char a; cin >> a;
+
+  cout << ((a >= 'a' && a <= 'z') ? "a" : "A") << endl;
+}
