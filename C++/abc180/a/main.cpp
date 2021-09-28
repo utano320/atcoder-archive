78bef609772f6cecd98be6e4b303e2eeec148a59
Mitsuo Utano <utano320@gmail.com>
2021-09-28 14:45:03 +0900

    ABC180 A

---
+++ C++/abc180/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n, a, b ; cin >> n >> a >> b;
+  
+  cout << n - a + b << endl;
+}
