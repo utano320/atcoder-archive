f79fcc214090ee00bed03d7890fc2ce0d0c2bc6b
Mitsuo Utano <utano320@gmail.com>
2021-10-19 20:34:12 +0900

    ABC211 A

---
+++ C++/abc211/a/main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  double a, b; cin >> a >> b;
+
+  cout << fixed << setprecision(7) << (a + 2 * b) / 3 << endl;
+}
