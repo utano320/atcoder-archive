c16122ae471578cf757cfcea6343d41aa3202069
Mitsuo Utano <utano320@gmail.com>
2021-11-02 16:52:59 +0900

    ABC218 A

---
+++ C++/abc218/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int n; cin >> n;
+  string s; cin >> s;
+  
+  cout << ((s[n-1] == 'o') ? "Yes" : "No") << endl;
+}
