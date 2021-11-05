cdb56953b99f52123b61f91d662929013cc31c74
Mitsuo Utano <utano320@gmail.com>
2021-11-05 23:34:46 +0900

    ABC219 A

---
+++ C++/abc219/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  int x; cin >> x;
+
+  if (x < 40) {
+    cout << 40 - x << endl;
+  } else if (x < 70) {
+    cout << 70 - x << endl;
+  } else if (x < 90) {
+    cout << 90 - x << endl;
+  } else {
+    cout << "expert" << endl;
+  }
+}
