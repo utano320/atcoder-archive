61bb99f7e7e9aa285b9d4c31ab76576c7af058dc
Mitsuo Utano <utano320@gmail.com>
2021-12-09 18:21:41 +0900

    ABC154 B

---
+++ C++/abc154/b/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string s; cin >> s;
+
+  rep(i, s.size()) {
+    cout << 'x';
+  }
+  // output
+  cout << endl;
+}
