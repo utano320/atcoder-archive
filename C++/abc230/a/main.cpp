fbcc75296f4c520ec3069e3ffd42c0336fd5e704
Mitsuo Utano <utano320@gmail.com>
2022-02-10 07:35:47 +0900

    ABC230 A

---
+++ C++/abc230/a/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int64_t solve(int n, const vector<int64_t>& a) {
+  int64_t ans;
+
+  return ans;
+}
+
+int main() {
+  // input
+  int n; cin >> n;
+
+  if (n >= 42) n++;
+
+  // output
+  cout << "AGC" << setfill('0') << right << setw(3) << n << endl;
+}
