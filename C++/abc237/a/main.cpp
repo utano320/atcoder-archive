e6503b2acff53cf8835dc7258640e3600097268e
Mitsuo Utano <utano320@gmail.com>
2022-02-18 07:33:37 +0900

    ABC237 A

---
+++ C++/abc237/a/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int64_t solve(int n, const vector<int64_t>& a) {
+  int64_t ans;
+
+  return ans;
+}
+
+int main() {
+  // input
+  long long n; cin >> n;
+
+  long long m = pow(2, 31);
+
+  // output
+  cout << ((-m <= n && n < m) ? "Yes" : "No") << endl;
+}
