1c9c845f859da98509f9a0d632f237839cd0ba5d
Mitsuo Utano <utano320@gmail.com>
2022-03-12 10:46:16 +0900

    ABC090 A

---
+++ C++/abc090/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string a, b, c;
+  cin >> a;
+  cin >> b;
+  cin >> c;
+
+  // output
+  cout << a[0] << b[1] << c[2] << endl;
+}
