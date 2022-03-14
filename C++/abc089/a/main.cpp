23be5ae996da22d1c83f06915b8bdd2ad7b513ea
Mitsuo Utano <utano320@gmail.com>
2022-03-15 07:53:49 +0900

    ABC089 A

---
+++ C++/abc089/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+  int ans = 0;
+  if (n > 3) {
+    ans = n / 3;
+  }
+
+  // output
+  cout << ans << endl;
+}
