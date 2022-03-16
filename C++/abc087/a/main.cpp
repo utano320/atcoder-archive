03bbc1d51243c16428a6e1868aac9f738701d5f7
Mitsuo Utano <utano320@gmail.com>
2022-03-17 07:31:43 +0900

    ABC087 A

---
+++ C++/abc087/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int x, a, b; cin >> x >> a >> b;
+
+  // output
+  cout << (x - a) % b << endl;
+}
