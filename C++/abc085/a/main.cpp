27a2cdf27ceb25aa445a47348fc573c548c94105
Mitsuo Utano <utano320@gmail.com>
2022-03-18 13:18:59 +0900

    ABC085 A

---
+++ C++/abc085/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+
+int main() {
+  // input
+  string s; cin >> s;
+  // output
+  cout << "2018/01/" << s.substr(8, 2) << endl;
+}
