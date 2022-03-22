a4a3b3365831199d7da27892ac48c730b360347e
Mitsuo Utano <utano320@gmail.com>
2022-03-23 07:35:25 +0900

    ABC244 A

---
+++ C++/abc244/a/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+  string s; cin >> s;
+
+  // output
+  cout << s[n-1] << endl;
+}
