277782b4532885dd96e1592ae60ea48e400233e5
Mitsuo Utano <utano320@gmail.com>
2022-03-30 08:05:32 +0900

    ABC080 A

---
+++ C++/abc080/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n, a, b; cin >> n >> a >> b;
+  // output
+  cout << min(a * n, b) << endl;
+}
