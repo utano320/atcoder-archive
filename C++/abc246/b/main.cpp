31e530c1a3acb4eaf1dd5e69240e3ba1d18d28ef
Mitsuo Utano <utano320@gmail.com>
2022-04-03 09:30:49 +0900

    ABC246 B

---
+++ C++/abc246/b/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int64_t solve(int n, const vector<int64_t>& a) {
+  int64_t ans;
+
+  return ans;
+}
+
+int main() {
+  // input
+  double a, b; cin >> a >> b;
+  double c = sqrt(a * a + b * b);
+  // output
+  cout << fixed << setprecision(6) << a / c;
+  cout << ' ';
+  cout << fixed << setprecision(6) << b / c;
+}
