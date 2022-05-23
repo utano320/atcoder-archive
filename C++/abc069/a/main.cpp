4fc4ee0f6feb17e05da48ca86d61a1e8c315da6e
Mitsuo Utano <utano320@gmail.com>
2022-05-23 17:56:03 +0900

    ABC069 A

---
+++ C++/abc069/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n, m; cin >> n >> m;
+  // output
+  cout << (n - 1) * (m - 1) << endl;
+}
