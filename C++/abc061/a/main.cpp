52b0eaa9f39e280060b6ed6010bd59c209027b7d
Mitsuo Utano <utano320@gmail.com>
2022-05-30 05:18:36 +0900

    ABC061 A

---
+++ C++/abc061/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int a, b, c; cin >> a >> b >> c;
+
+  // output
+  cout << ((c >= a && c <= b) ? "Yes" : "No") << endl;
+}
