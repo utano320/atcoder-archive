c51f7506175202fc49c87872435bbd469c6820d1
Mitsuo Utano <utano320@gmail.com>
2022-06-09 12:43:02 +0900

    ABC051 A

---
+++ C++/abc051/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string s; cin >> s;
+
+  s[5] = ' ';
+  s[13] = ' ';
+
+  // output
+  cout << s << endl;
+}
