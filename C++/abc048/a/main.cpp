ccb4608b1c9b7d4db7a23f45da66bcf33f181eb9
Mitsuo Utano <utano320@gmail.com>
2022-06-13 04:14:48 +0900

    ABC048 A

---
+++ C++/abc048/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string a, b, c; cin >> a >> b >> c;
+
+  // output
+  cout << a[0] << b[0] << c[0] << endl;
+}
