1ae253afed7c3b9569d10dfdd5d770b6ba44e881
Mitsuo Utano <utano320@gmail.com>
2022-06-20 05:02:30 +0900

    ABC038 A

---
+++ C++/abc038/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string s; cin >> s;
+
+  // output
+  cout << ((s[s.size() - 1] == 'T') ? "YES" : "NO") << endl;
+}
