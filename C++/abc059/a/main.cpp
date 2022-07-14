d51fc3835f7b90c044b34e0bd725b222accd08e9
Mitsuo Utano <utano320@gmail.com>
2022-07-15 08:49:14 +0900

    ABC059 A

---
+++ C++/abc059/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string a, b, c; cin >> a >> b >> c;
+  char x = a[0] - 32;
+  char y = b[0] - 32;
+  char z = c[0] - 32;
+
+  // output
+  cout << x << y << z << endl;
+}
