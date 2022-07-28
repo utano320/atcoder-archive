bfbce4fda5aeed3b01718568ba71c9879ebdcd4f
Mitsuo Utano <utano320@gmail.com>
2022-07-29 04:31:09 +0900

    ABC049 B

---
+++ C++/abc049/b/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int h, w; cin >> h >> w;
+  rep(i, h) {
+    string s;
+    cin >> s;
+    cout << s << endl;
+    cout << s << endl;
+  }
+}
