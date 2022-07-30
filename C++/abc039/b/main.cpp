9ac09ffcd1b44aeeb7c5096f0e19f644e2c504ac
Mitsuo Utano <utano320@gmail.com>
2022-07-31 04:19:20 +0900

    ABC039 B

---
+++ C++/abc039/b/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  double x; cin >> x;
+  double ans = sqrt(sqrt(x));
+
+  // output
+  cout << ans << endl;
+}
