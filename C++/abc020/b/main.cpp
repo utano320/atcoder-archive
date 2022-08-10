b7053177ff26772b1eff5916adf16aa3b69e2dcf
Mitsuo Utano <utano320@gmail.com>
2022-08-10 11:19:01 +0900

    ABC020 B

---
+++ C++/abc020/b/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string a, b; cin >> a >> b;
+  int ans = stoi(a + b) * 2;
+
+  // output
+  cout << ans << endl;
+}
