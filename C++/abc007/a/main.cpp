de8cbe29bfe6dc11415ccdd12770bb0056957d89
Mitsuo Utano <utano320@gmail.com>
2022-08-29 04:24:46 +0900

    ABC007 A

---
+++ C++/abc007/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+
+  // output
+  cout << n - 1 << endl;
+}
