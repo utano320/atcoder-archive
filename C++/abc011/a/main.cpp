00cfa9c04255febccb793be78d1025eec54e0b6e
Mitsuo Utano <utano320@gmail.com>
2022-09-02 10:30:41 +0900

    ABC011 A

---
+++ C++/abc011/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int n; cin >> n;
+
+  // solve
+  int ans = (n == 12) ? 1 : n + 1;
+
+  // output
+  cout << ans << endl;
+}
