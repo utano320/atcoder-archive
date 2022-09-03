5bb7deb6b118dd1d7f45957263e5ff5b933ad579
Mitsuo Utano <utano320@gmail.com>
2022-09-04 05:19:12 +0900

    ABC013 A

---
+++ C++/abc013/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  char x; cin >> x;
+  // output
+  cout << x - 'A' + 1 << endl;
+}
