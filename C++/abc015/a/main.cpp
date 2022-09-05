4e1ac561e8c328e0849b010ad44e2c2ed4fae9b4
Mitsuo Utano <utano320@gmail.com>
2022-09-06 04:38:08 +0900

    ABC015 A

---
+++ C++/abc015/a/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string a, b; cin >> a >> b;
+
+  if (a.size() > b.size()) {
+    cout << a << endl;
+  } else {
+    cout << b << endl;
+  }
+}
