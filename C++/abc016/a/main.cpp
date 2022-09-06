6cbeb94176279ad3c45c70af621ad05ff44be000
Mitsuo Utano <utano320@gmail.com>
2022-09-07 08:07:12 +0900

    ABC0q6 A

---
+++ C++/abc016/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int m, d; cin >> m >> d;
+
+  // output
+  cout << ((m % d == 0) ? "YES" : "NO") << endl;
+}
