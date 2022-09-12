98fd2039ed2707f6df07eb4dfb9537c1d742d3f1
Mitsuo Utano <utano320@gmail.com>
2022-09-12 20:30:37 +0900

    ABC266 A

---
+++ C++/abc266/a/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  string s; cin >> s;
+
+  int i = s.size() / 2;
+
+  // output
+  cout << s[i] << endl;
+}
