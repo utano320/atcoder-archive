0af7e1a2c4bb19eb10cefa8f277b17c46c2ddc1b
Mitsuo Utano <utano320@gmail.com>
2022-09-20 21:11:03 +0900

    ABC269 A

---
+++ C++/abc269/a/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main() {
+  // input
+  int a, b, c, d; cin >> a >> b >> c >> d;
+  cout << (a + b) * (c - d) << endl;
+  cout << "Takahashi" << endl;
+}
